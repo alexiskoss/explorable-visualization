f61f3f714802cea0ff9e4475289cbb436f8adebe
Lisa Koss <lisakoss@uw.edu>
2018-03-04 13:34:22 -0800

    Dynamic grid resizes based on grid size

--- js/index.js
+++ js/index.js
@@ -7,20 +7,28 @@ $("#columns").on("input", function(e) {
 });
 
 function gridSize() {
-    let rows = $("#rows").val();
-    let columns = $("#columns").val();
+    let rows = parseInt($("#rows").val());
+    let columns = parseInt($("#columns").val());
     let divideVal = 0;
     let boardSize = 0;
+    let value = 0;
 
-    if(rows > columns) {
-        boardSize = 484;
-        divideVal = rows;
-    } else {
+    console.log(columns)
+    console.log(rows)
+    console.log(rows > columns)
+    if(columns > rows) {
+        console.log("cols<rows")
         boardSize = 528;
         divideVal = columns;
-    }
+        value = Math.floor(boardSize/divideVal);
+    } else {
+        console.log("rows>cols")
+        boardSize = 484;
+        divideVal = rows;
+        value = Math.floor(boardSize/divideVal);
+    } 
 
-    $("#grid").empty();
+    /*$("#grid").empty();
     for(let i = 1; i <= rows; i++) {
         for(let j = 1; j <= columns; j++) {
             if(j == 1) {
@@ -30,6 +38,22 @@ function gridSize() {
             $(`#row${i}`).append("<div></div>")
             $(`#row${i} div`).css("height", Math.floor(boardSize/divideVal));
             $(`#row${i} div`).css("width", Math.floor(boardSize/divideVal));
+        }
+    }*/
+
+    $("#grid-bg").empty();
+    for(let i = 1; i <= rows; i++) {
+        for(let j = 1; j <= columns; j++) {
+            if(j == 1) {
+                $("#grid-bg").append(`<div id="row${i}"></div>`)
+                $(`#row${i}`).css("height", value);
+            }
+            $(`#row${i}`).append(`<div id="col${j}"><span><img src="svg/coral.svg" height="${value / 2}px" width="${value / 2}px"></span></div>`)
+            $(`#row${i} #col${j} span img`).css("margin-top", "50%");
+            $(`#row${i} #col${j}`).css("height", (value) - 2);
+            $(`#row${i} #col${j}`).css("width", (value) - 2);
+            $(`#row${i} #col${j}`).css("top", value * (i - 1));
+            $(`#row${i} #col${j}`).css("left", value * (j - 1));
         }
     }
 }

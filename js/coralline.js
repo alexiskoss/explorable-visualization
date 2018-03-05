78b297e759aab785f55a1f5ea2e195b1dbbca190
Alexis Koss <alexis30@uw.edu>
2018-03-05 10:19:30 -0800

    working on html page

--- js/coralline.js
+++ js/coralline.js
@@ -1,34 +1,47 @@
+sr.reveal('#coral-line', { duration: 1000, delay: 1000 });
+
+var waypoint = new Waypoint({
+    element: document.getElementById('coral-map'),
+    handler: function(direction) {
+        d3.select("#coral-line-title h1").remove();
+        d3.select("#coral-line svg").remove();
+        intialize();
+    }
+  })
+
 /* http://bl.ocks.org/markmarkoh/8700606 */
 
-var margin = {top: 80, right: 80, bottom: 80, left: 80},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+function intialize() {
+    var margin = {top: 80, right: 80, bottom: 80, left: 80},
+        width = 900 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
 
-// Scales and axes. Note the inverted domain for the y-scale: bigger is up!
-var x = d3.scale.linear().range([0, width]),
-    y = d3.scale.linear().range([height, 0]),
-    xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true).tickFormat(d3.format("d")),
-    yAxis = d3.svg.axis().scale(y).ticks(4).orient("right");
+    // Scales and axes. Note the inverted domain for the y-scale: bigger is up!
+    var x = d3.scale.linear().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true).tickFormat(d3.format("d")),
+        yAxis = d3.svg.axis().scale(y).ticks(4).orient("right");
 
-// An area generator, for the light fill.
-var area = d3.svg.area()
-    .interpolate("monotone")
-    .x(function(d) { return x(d.year); })
-    .y0(height)
-    .y1(function(d) { return y(parseFloat(d.percentage_affected)); });
+    // An area generator, for the light fill.
+    var area = d3.svg.area()
+        .interpolate("monotone")
+        .x(function(d) { return x(d.year); })
+        .y0(height)
+        .y1(function(d) { return y(parseFloat(d.percentage_affected)); });
 
-// A line generator, for the dark stroke.
-var line = d3.svg.line()
-    .interpolate("monotone")
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(parseFloat(d.percentage_affected)); });
+    // A line generator, for the dark stroke.
+    var line = d3.svg.line()
+        .interpolate("monotone")
+        .x(function(d) { return x(d.year); })
+        .y(function(d) { return y(parseFloat(d.percentage_affected)); });
+
+    //tool tip
+    tip = d3.tip().attr('class', 'd3-tip').html(function(d) { 
+        return "Cases: " +  d.percentage_affected + "<br/>" + "Bleaching Severity: " + d.bleaching_severity + "<br/>" + "Year Reported: " + d.year});
 
 d3.csv("data/CoralBleachingCleaned.csv", function(error, data) {
 
-    
-
-  // Filter to one symbol; the S&P 500.
   var high = data.filter(function(d) {
     return d.bleaching_severity == "HIGH";
   });
@@ -105,56 +118,117 @@ d3.csv("data/CoralBleachingCleaned.csv", function(error, data) {
   y.domain([0, 260]);
 
   // Add an SVG element with the desired dimensions and margin.
-  var svg = d3.select("#coral-line").append("svg")
+  var svgLine = d3.select("#coral-line").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr("transform", "translate(" + 30 + "," + margin.top + ")")
 
   // Add the clip path.
-  svg.append("clipPath")
+  svgLine.append("clipPath")
       .attr("id", "clip")
     .append("rect")
       .attr("width", width)
       .attr("height", height);
 
   // Add the x-axis.
-  svg.append("g")
+  svgLine.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+      .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", 420)
+        .attr("y", 40)
+        .style("text-anchor", "end")
+        .text("Year");
 
   // Add the y-axis.
-  svg.append("g")
+  svgLine.append("g")
       .attr("class", "y axis")
-      .attr("transform", "translate(" + width + ",0)")
-      .call(yAxis);
+      .attr("transform", "translate(" + (width + 10) + "," + -5 + ")")
+      .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 45)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("# Of Bleaching Cases Reported")
 
+/*http://bl.ocks.org/Matthew-Weber/5645518*/
+svgLine.selectAll('.line')
+.data([highArray, mediumArray, lowArray, noArray])
+.enter()
+    .append('path')
+    .attr('class', 'line')
+    .style('stroke', function(d) {
+        if(d == highArray) {
+            return "#ec4e4e";
+        } else if(d == mediumArray) {
+            return "#ee9a54";
+        } else if(d == lowArray) {
+            return "#f6df53";
+        } else if(d == noArray) {
+            return "#00994C";
+        }
+    })
+    .on("mouseover", function (d) {                                  
+        d3.select(this)                          //on mouseover of each line, give it a nice thick stroke
+        .style("stroke-width",'6px')
+        var selectLines = $('.line').not(this);     //select all the rest of the lines, except the one you are hovering on and drop their opacity
+        d3.selectAll(selectLines)
+            .style("opacity", 0.3);
+    })
+    .on("mouseout",	function(d) {        //undo everything on the mouseout
+        d3.select(this)
+            .style("stroke-width",'2.5px');
+        var selectLines = $('.line').not(this);
+        d3.selectAll(selectLines)
+            .style("opacity", 1);
+    })
+    .attr('clip-path', 'url(#clip)')
+    .attr('d', function(d) {
+        return line(d);
+    });
 
-  svg.selectAll('.line')
-    .data([highArray, mediumArray, lowArray, noArray])
-    .enter()
-      .append('path')
-        .attr('class', 'line')
-        .style('stroke', function(d) {
-            if(d == highArray) {
-                return "#ec4e4e";
-            } else if(d == mediumArray) {
-                return "#ee9a54";
-            } else if(d == lowArray) {
-                return "#f6df53";
-            } else if(d == noArray) {
-                return "#00994C";
-            }
-        })
-        .attr('clip-path', 'url(#clip)')
-        .attr('d', function(d) {
-          return line(d);
-        })
-        
+//creates legend for the line graph
+var legend = svgLine.selectAll(".legend")
+    .data(["#ec4e4e", "#ee9a54", "#f6df53", "#00994C"])
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(" + -600 + ',' + i * 20 + ")"; });
+
+legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", function(d) {
+        return d;
+    });
+
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { 
+        if(d == "#ec4e4e") {
+            return "High"
+        } else if(d == "#ee9a54") {
+            return "Med"
+        } else if(d == "#f6df53") {
+            return "Low"
+        } else {
+            return "None"
+        }
+    });
+
+d3.select("#coral-line-title").append('h1') 
+        .text('Number of Coral Bleaching Cases per Year');
 
   /* Add 'curtain' rectangle to hide entire graph */
-  var curtain = svg.append('rect')
+  var curtain = svgLine.append('rect')
     .attr('x', -1 * width)
     .attr('y', -1 * height)
     .attr('height', height)
@@ -163,18 +237,8 @@ d3.csv("data/CoralBleachingCleaned.csv", function(error, data) {
     .attr('transform', 'rotate(180)')
     .style('fill', '#ffffff')
 
-  /* Optionally add a guideline */
-  var guideline = svg.append('line')
-    .attr('stroke', '#333')
-    .attr('stroke-width', 0)
-    .attr('class', 'guide')
-    .attr('x1', 1)
-    .attr('y1', 1)
-    .attr('x2', 1)
-    .attr('y2', height)
-
   /* Create a shared transition for anything we're animating */
-  var t = svg.transition()
+  var t = svgLine.transition()
     .delay(750)
     .duration(6000)
     .ease('linear')
@@ -187,12 +251,6 @@ d3.csv("data/CoralBleachingCleaned.csv", function(error, data) {
 
   t.select('rect.curtain')
     .attr('width', 0);
-  t.select('line.guide')
-    .attr('transform', 'translate(' + width + ', 0)')
-
-  d3.select("#show_guideline").on("change", function(e) {
-    guideline.attr('stroke-width', this.checked ? 1 : 0);
-    curtain.attr("opacity", this.checked ? 0.75 : 1);
-  })
 
 });
+}
--- js/coralmap.js
+++ js/coralmap.js
@@ -1,8 +1,13 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.coral-container p', { duration: 1000, delay: 300 }, 100);
+sr.reveal('#coral-map', { duration: 1000, delay: 1000 });
+
 var dataset; //original dataset
 var filterData = "";
 let prevYear;
 let year = 1983;
-let timer = setInterval(automaticMap, 100);
+let timer;
 
 /*http://www.d3noob.org/2013/03/a-simple-d3js-map-explained.html*/
 var width = '100%',
@@ -30,6 +35,32 @@ var g = svg.append("g");
 //scale for circle data points
 var circleScale = d3.scale.sqrt().domain([0, 100]).range([0, 10]);
 
+var mapLegend = d3.select("#coral-map-legend").append("svg")
+    .attr("width", '100%')
+    .attr("height", 80);
+
+svg
+    .data(["Percentage of coral affected:"])
+    .append("text")
+        .text(function (d) { return d; })
+        .attr("x", 10)
+        .attr("y", 20)
+
+//legend for circle sizes
+svg.append("g")
+  .attr("class", "legendSize")
+  .attr("transform", "translate(20, 40)");
+
+var legendSize = d3.legend.size()
+  .scale(circleScale)
+  .shape('circle')
+  .shapePadding(25)
+  .labelOffset(20)
+  .orient('horizontal');
+
+svg.select(".legendSize")
+  .call(legendSize);
+
 // load and display the World
 d3.json("json/world-110m2.json", function(error, topology) {
     if(error) throw error;
@@ -45,8 +76,13 @@ d3.json("json/world-110m2.json", function(error, topology) {
             .append("path")
             .attr("d", path)
 
-        automaticMap();
-       // drawVis(dataset);
+            var waypoint = new Waypoint({
+                element: document.getElementById('coral-description'),
+                handler: function(direction) {
+                    timer = setInterval(automaticMap, 1000);
+                    automaticMap();
+                }
+              })
     });               
 });
 
@@ -116,19 +152,32 @@ function drawVis(ndata) {
 
 //zooming and panning
 var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 5])
     .on("zoom",function() {
         g.attr("transform","translate("+ 
             d3.event.translate.join(",")+")scale("+d3.event.scale+")");
         g.selectAll("path")  
             .attr("d", path.projection(projection)); 
+    
 });
 
 svg.call(zoom)
+    .on("wheel", function() { d3.event.preventDefault(); });
 
-var coralmap = d3.select('#coral-map')
+var coralmaptitle = d3.select('#coral-map-title');
+var coralmapdesc = d3.select('#coral-map-desc');
+var coralmapslider = d3.select("#coral-map-slider");
 
-coralmap.append('p').text('Select year:')
-coralmap.append('input')
+coralmapdesc.append('p').style("text-align", "center").append('button').text('Reset map animation')
+    .on('click', function() {
+        clearInterval(timer)
+        timer = setInterval(automaticMap, 1000);
+        year = 1983;
+        automaticMap();
+    });
+
+coralmapslider .append('input')
+  .style("text-align", "center")
   .attr('class','inputRadius')
   .attr('id', 'yearInput')
   .attr('type','range')
@@ -136,16 +185,30 @@ coralmap.append('input')
   .attr('max',2010)
   .attr('step',1)
   .attr('value',2010)
+  .style("clear", 'both')
+  .style("margin-top", "18px")
   .on('change',function() {
-    coralmap.selectAll('h1').remove()
-    coralmap.append('h1') 
-        .attr('id', "#coral-map-title")
+    coralmaptitle.selectAll('h1').remove()
+    coralmaptitle.append('h1') 
         .text('Coral Bleaching in ' + document.getElementById("yearInput").value);
+        
+    coralmapslider .selectAll('p').remove();
+    coralmapslider .append('p')
+        .text('Select year: ' + document.getElementById("yearInput").value)
+        .style("float", 'left')
+        .attr("id", "#range-desc");
+
   	filterUpdate(dataset);
   });
 
+coralmapslider .append('p')
+    .text('Select year: ' + document.getElementById("yearInput").value)
+    .style("float", 'left')
+    .attr("id", "#range-desc");
+
 function filterUpdate(dataset) {
     let currentYear = document.getElementById("yearInput").value;
+
     filterData = dataset;
 
     if(currentYear != '2010') {
@@ -155,11 +218,31 @@ function filterUpdate(dataset) {
     drawVis(filterData);
 }
 
+function linkUpdate(year) {
+    let currentYear = year;
+    filterData = dataset;
+
+    d3.select("#yearInput")
+        .attr('value', year);
+
+    coralmaptitle.selectAll('h1').remove()
+    coralmaptitle.append('h1') 
+        .text('Coral Bleaching in ' + document.getElementById("yearInput").value);
+
+    if(currentYear != '2010') {
+        filterData = filterData.filter(d => d.year <= currentYear); 
+    }
+
+    let coralMap = document.getElementById('coral-map-title');
+    $("#coral-map-title").get(0).scrollIntoView();
+    
+    drawVis(filterData);
+}
+
 function automaticMap() {
     if(year <= 2010) {
-        coralmap.selectAll('h1').remove()
-        coralmap.append('h1') 
-            .attr('id', "#coral-map-title")
+        coralmaptitle.selectAll('h1').remove()
+        coralmaptitle.append('h1') 
             .text('Coral Bleaching in ' + year)
 
         mapAnimation(dataset, year);
@@ -180,3 +263,35 @@ function mapAnimation(dataset, year) {
     
     drawVis(filterData);
 }
+
+//creates legend for the map graph
+var legend = svg.selectAll(".legend")
+    .data(["#ec4e4e", "#ee9a54", "#f6df53", "#00994C"])
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(" + 320 + ',' + i * 20 + ")"; });
+
+legend.append("rect")
+    .attr("x", 30 - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", function(d) {
+        return d;
+    });
+
+legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { 
+        if(d == "#ec4e4e") {
+            return "High"
+        } else if(d == "#ee9a54") {
+            return "Med."
+        } else if(d == "#f6df53") {
+            return "Low"
+        } else {
+            return "No bleaching"
+        }
+    });
